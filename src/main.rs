eec75335f54e73d591556918110b7b34a5754cb0
mikesmithlab <mikesmithlab@gmail.com>
2026-05-10 21:07:31 +0100

    Stage 2 BYOS complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::io::Read;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut cmd_buffer = String::new();
+    let _ = io::stdin().read_line(&mut cmd_buffer);
+    
+
+    println!("{}: command not found", cmd_buffer.trim());
     
 }
