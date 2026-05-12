de8c1cb215318322d65768843aa6a52eefa0dca9
Mike Smith <mike.smith.code@gmail.com>
2026-05-12 20:13:35 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 use std::path::PathBuf;
 use std::fs;
 use std::os::unix::fs::MetadataExt;
+use std::process;
 
 const BUILTINS: &[&str] = &["exit", "type", "echo"];
 
@@ -13,10 +14,17 @@ fn is_builtin(cmd: &str)->bool{
     BUILTINS.contains(&cmd)
 }
 
+
+
 enum Command{
     Exit,
-    Echo(Vec<String>),
-    Type(String),
+    Builtin{cmd: String, 
+            args: Vec<String>,
+            },
+    External{cmd: String,
+             path: PathBuf,
+             args: Vec<String>,
+            },
     Error(String),
 }
 
@@ -31,22 +39,29 @@ fn prompt()-> Result<Vec<String>, std::io::Error>{
     Ok(input)
 }
 
-/// Figures out which enum variant we have and adds args to variant if appropriate
+
 fn parse(mut buffer: Vec<String>)->Command{
     if buffer.is_empty(){return Command::Error(String::new());}
 
-    let cmd_name = buffer.remove(0);
-
-    match cmd_name.as_str(){
-        "exit" => Command::Exit,
-        "echo" => Command::Echo(buffer),
-        "type" => if buffer.is_empty() {
-                        Command::Error("type: missing argument".to_string())
-                    } else {
-                        Command::Type(buffer.remove(0))
-                    }
-        _ => Command::Error(format!("{}: command not found", cmd_name)),
+    let cmd = buffer.remove(0);
+    
+    if cmd.as_str() == "exit"{
+        return Command::Exit;
     }
+
+    let args = buffer;
+    if is_builtin(cmd.as_str()){
+        return Command::Builtin {cmd, args};
+    }
+    
+    let path = get_path_env();
+    let found_exe = search_for_executables(&cmd, &path);
+
+    match found_exe{
+        Some(path) =>  Command::External{cmd, path, args},
+        None =>  Command::Error(format!("{}: command not found", cmd)),
+    }
+    
 }
 
 
@@ -59,14 +74,14 @@ fn is_executable(filepath: &PathBuf)-> bool{
    // setting that != 0 returns true if any of the bits are nonzero.
    match attr{
     Ok(attr) => attr.is_file() && ((attr.mode() & 0o111) != 0),
-    Err(e) => false,
+    Err(_e) => false,
    }   
 }   
 
 
-fn search_for_executables(cmd: String, paths: Vec<PathBuf>)->Option<PathBuf>{
-    for path in &paths{
-        let filepath = path.join(&cmd);
+fn search_for_executables(cmd: &str, paths: &Vec<PathBuf>)->Option<PathBuf>{
+    for path in paths{
+        let filepath = path.join(cmd);
         if is_executable(&filepath){
             return Some(filepath);
         } 
@@ -82,28 +97,49 @@ fn get_path_env()-> Vec<PathBuf>{
     }
 }
 
+fn handle_echo(args: Vec<String>){
+    println!("{}", args.join(" "));
+}
+
+fn handle_type(args: Vec<String>){
+    let cmd_arg = args[0].clone();
+    if is_builtin(&cmd_arg)
+        {
+            println!("{} is a shell builtin", cmd_arg);
+        }
+        else
+        { 
+            let paths = get_path_env();
+            
+            let file = search_for_executables(&cmd_arg.to_string(), &paths);
+            match file{
+                Some(filepath) => println!("{} is {}", cmd_arg.to_string(), filepath.display()),
+                None => println!("{}: not found", cmd_arg),
+                }                                
+        }
+}
+
+fn handle_external(path: PathBuf, args: Vec<String>){
+    let mut process_cmd = process::Command::new(path);
+    process_cmd.args(args);
+    let response = process_cmd.output().expect("failed to execute process");
+    io::stdout().write_all(&response.stdout).expect("write process response to stdout");
+}
+
 /// Execute the command
 fn run_command(cmd: Command){
     match cmd{
         Command::Exit => std::process::exit(0),
-        Command::Echo(args) => println!("{}", args.join(" ")),
-        Command::Type(arg) => {if is_builtin(&arg)
-                                {
-                                    println!("{} is a shell builtin", arg);
-                                }
-                                else
-                                { 
-                                    let paths = get_path_env();
-                                    let cmd = arg.clone();
-                                    let file = search_for_executables(arg.to_string(), paths);
-                                    match file{
-                                        Some(filepath) => println!("{} is {}", cmd.to_string(), filepath.display()),
-                                        None => println!("{}: not found", cmd),
-                                        }                                
-                                }
-                            },
-        Command::Error(arg) => println!("{}", arg),
-                        }
+        Command::Builtin{cmd, args} => {
+                    match cmd.as_str(){
+                        "echo" => handle_echo(args),
+                        "type" => handle_type(args),
+                        _ => unreachable!("{} not implemented", cmd),
+                    }                                
+                },
+        Command::External{cmd, path, args} => handle_external(path, args),
+        Command::Error(msg) => println!("{}", msg),
+  }
 
 }
 
@@ -113,9 +149,6 @@ fn main() {
         let buffer: Vec<String> = prompt().expect("error with input");
         let cmd = parse(buffer);
         run_command(cmd);
-
-        
-        
 
     }
     
